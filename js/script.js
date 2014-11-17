eabe0a40ae85dac48e7defa7dc800de8c8f9a07c
Aakash Khandelwal <aakash_284@live.com>
2014-11-17 12:13:46 +0530

    implemented NYT apis

--- js/script.js
+++ js/script.js
@@ -17,10 +17,24 @@ function loadData() {
     var address = street + ', ' + city;
     $greeting.text('So, you want ot visit '+ address + '?');
 
-    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=1200x720&location=' + address + '';
     $body.append('<img class="bgimg" src=" '+ streetviewUrl + '">');
 
-    // YOUR CODE GOES HERE!
+    // YOUR CODE GOES HERE! NYTimes
+
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + city + '&sort=newest&api-key=202afa73c8bb09340e53c42de03600d8:16:70196210';
+    $.getJSON(nytimesUrl, function(data){
+        $nytHeaderElem.text('New York Times Articles About' + city);
+
+        articles = data.response.docs;
+        for (var i=0; i<articles.length; i++){
+            var article = articles[i];
+            $nytElem.append('<li class = "article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>'+ article.snippet + '</p>'+
+                '</li>');
+        };
+    })
 
     return false;
 };
